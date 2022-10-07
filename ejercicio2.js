c8d03490720ec001382a00f91b6a5121ec8fa3fd
ENZCode <93941427+ENZCode@users.noreply.github.com>
2022-10-07 17:39:47 +0200

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+const lista = ["Enric", "24", "true", new Date(1998, 3, 16), 
+    libro = {
+    titulo: "El libro del ego",
+    autor: "Osho",
+    fecha: new Date(1994, 7, 22),
+    url: "https://www.casadellibro.com/libro-el-libro-del-ego/9788499892702/1872776",
+    }
+]
+
+console.log(lista);
